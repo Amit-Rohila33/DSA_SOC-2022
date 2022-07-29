93c15f711690ae55468c49a90451d8172d3771cd
Amit-Rohila33 <210010008@iitb.ac.in>
2022-07-28 23:03:14 -0700

    Week 5 committed which contanied Strings and Linked list

---
+++ Week_5_Strings_and_Linked_list/Strings/understanding_strings/convenient_way_print_string.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    // int a = 4;
+    // printf("%d", a);
+
+    // char *ptr = "Amit Kumar";
+    char ptr[] = "Amit Kumar";
+    printf("%s", ptr);
+    return 0;
+}
---
+++ Week_5_Strings_and_Linked_list/Strings/understanding_strings/intro_to_strings.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    // char str[] = {'A', 'm', 'i', 't', '\0'};
+    char str[] = "Amit";
+    
+    return 0;
+}
