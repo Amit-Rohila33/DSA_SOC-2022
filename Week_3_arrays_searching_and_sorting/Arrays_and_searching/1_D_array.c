36a1d796ac9d13892e5dc9770fa24de70b1b55e6
Amit-Rohila33 <210010008@iitb.ac.in>
2022-07-28 23:01:51 -0700

    Week 3 committed Arrays,searching and sorting

---
+++ Week_3_arrays_searching_and_sorting/Arrays_and_searching/1_D_array.c
@@ -0,0 +1,29 @@
+/*This is a simple example of a one-dimensional array which I used 
+ to understand the array in one dimension. */
+
+ 
+
+
+
+
+#include<stdio.h>
+
+int main()
+{
+	//One dimensional array
+	int marks[10], sum=0;
+	printf("Enter marks of 10 students : \n\n");
+	for (int i=0;i<=9;i++)
+	{
+	printf("Marks of %d student : ", i+1);
+	scanf("%d", &marks[i]);
+	sum += marks[i];
+	}
+
+	int average = sum/10;
+	printf("\nThe average marks of 10 students are %d", average);
+	return 0;
+}
+
+
+
